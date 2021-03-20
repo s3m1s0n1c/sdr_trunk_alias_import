2d0715400297f913ee01f6e1b2402cf9d6c370e6
Sonic <s3m1s0n1c@gmail.com>
2021-03-20 21:24:25 +1100

    Add files via upload

---
+++ SDR_trunk_alias_import.py
@@ -0,0 +1,31 @@
+import csv
+
+csvFile = 'sample.csv'
+xmlFile = 'alias.xml'
+
+csvData = csv.reader(open(csvFile))
+xmlData = open(xmlFile, 'w')
+
+rowNum = 0
+for row in csvData:
+    if rowNum == 0:
+        tags = row
+        # replace spaces w/ underscores in tag names
+        #for i in range(len(tags)):
+         #   tags[i] = tags[i].replace(' ', '_')
+    else: 
+        xmlData.write("\n")
+
+        xmlData.write('   ' + '<alias list="' + row[0] + '" ' + 'group="' + row[1] + '" ' + 'iconName="' + row[2] + '" ' +  'color="' + row[3] + '" ' + 'name="' + row[4] + '">')
+        xmlData.write('\n')
+	xmlData.write('     ' + '<id type="' + row[5] + '" ' + 'value="' + row[6] + '" ' + 'protocol="' + row[7] + '"/>')
+        xmlData.write('\n')
+ 	xmlData.write('   ' + '</alias>')
+
+
+            
+    rowNum +=1
+
+xmlData.close()
+
+
